ae145802d2acfd12bddea5ae6d3efca05406e9d5
dstuthi76 <dstuthi7@gmail.com>
2023-10-07 20:28:41 +0530

    initialization of header Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import About from './pages/About';
 import Signin from './pages/Signin';
 import Profile from './pages/profile';
 import SignOut from './pages/SignOut';
+import Header from './components/header'
 export default function  () {
   return <BrowserRouter>
+  <Header/>
   <Routes>
     <Route path="/" element={<Home/>}/>
     <Route path="/About" element={<About/>}/>
-    <Route path="/Signin" element={<Signin/>}/>
+    <Route path="/SignIn" element={<Signin/>}/>
     <Route path="/Profile" element={<Profile/>}/>
     <Route path="/SignOut" element={<SignOut/>}/>
   </Routes>
